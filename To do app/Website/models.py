c3d3c26161cbae3554361ad96431850785f7c274
Stefan Rose <rossstefan586@gmail.com>
2025-01-20 13:18:40 +0000

    Date and Time functionality added back to the updated structure as well as introduces a new functionality which enables user to edit the pre-existing to do task details and amend the changes to the database

--- To do app/Website/models.py
+++ To do app/Website/models.py
@@ -1,6 +1,8 @@
+from datetime import datetime #Imports the datetime from datetime module
 from Website import db
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     task = db.Column(db.String(300), unique = True)
-    complete = db.Column(db.Boolean, default = False) 
+    complete = db.Column(db.Boolean, default = False)
+    time_and_date = db.Column(db.DateTime, default=datetime.utcnow) #Adds the time and date column to Todo model, which stores the timestamp
--- To do app/Website/views.py
+++ To do app/Website/views.py
@@ -29,3 +29,18 @@ def delete(todo_id):
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("my_view.home"))
+
+#Route that enables editing of the specified task by its identification (Primary key)
+@my_view.route("/edit/<todo_id>", methods = ["GET", "POST"])
+def edit(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first() #Queries db for the task with the specified primary key
+    if not todo:
+        return "Todo not found", 404 #If said task does not exist, it will return a 404 error, also known as "Not Found"
+    
+    if request.method == "POST": #Method that handles the POST request to update the identified task
+        new_task = request.form.get("task") #Gets the edited task from the edit.html form
+        todo.task = new_task #Appends the edited task in the database
+        db.session.commit() #Commits task changes to the database
+        return redirect(url_for("my_view.home")) #redirects user back to the homepage once user has pressed "Save Changes"
+    
+    return render_template("edit.html", todo=todo) #renders the edit.html form, and passes the task to this template.
