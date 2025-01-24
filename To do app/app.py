7b6b18c1cba598ce5e89fd91491a7d95189bead8
Stefan Rose <rossstefan586@gmail.com>
2025-01-24 15:59:28 +0000

    Added requirements.txt and .gitignore for deployment readiness

--- To do app/app.py
+++ To do app/app.py
@@ -1,5 +1,7 @@
 from Website import create_app
 
+app = create_app()#Function runs the application via gunicorn, and build it from there not via python app.py script, which on the other hand is done from line 5 locally. 
+
 if __name__=="__main__":
     app = create_app()
     app.run(debug=True, port=8000) 
